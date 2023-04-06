9e016941fc4373d29cffa45b86d40b288814320a
cuiyiming1998 <1269567664@qq.com>
2023-04-06 18:48:13 +0800

    test: 添加getters actions测试

--- test/getters.test.ts
+++ test/getters.test.ts
@@ -1,4 +1,5 @@
 import { beforeEach, describe, expect, it } from 'vitest'
+import { computed } from 'vue-demi'
 import { createPinia, defineStore, setActivePinia } from '../src'
 
 describe('getters', () => {
@@ -25,5 +26,41 @@ describe('getters', () => {
     const store = useStore()
     expect(store.getName).toBe('young')
     expect(store.doubledAge).toBe(40)
+  })
+
+  it('should updates the value ', () => {
+    const store = useStore()
+    store.age = 15
+    expect(store.doubledAge).toBe(30)
+  })
+})
+
+// TODO: 对computed的支持
+describe('getters with setup option', () => {
+  beforeEach(() => {
+    setActivePinia(createPinia())
+  })
+
+  const useStore = defineStore('main', () => {
+    const name = 'young'
+    const age = 20
+
+    const doubledAge = computed(() => age * 2)
+    const getName = computed(() => name)
+
+    return { name, age, getName, doubledAge }
+  })
+
+  it.skip('should add getters to store ', () => {
+    const store = useStore()
+    expect(store.getName).toBe('young')
+    expect(store.doubledAge).toBe(40)
+  })
+
+  it.skip('should updates the value ', () => {
+    const store = useStore()
+    store.age = 15
+    console.log(store)
+    expect(store.doubledAge).toBe(30)
   })
 })
--- test/state.test.ts
+++ test/state.test.ts
@@ -1,5 +1,5 @@
 import { beforeEach, describe, expect, it } from 'vitest'
-import { computed } from 'vue-demi'
+import { computed, ref } from 'vue-demi'
 import { createPinia, defineStore, setActivePinia } from '../src'
 
 describe('options store', () => {
@@ -50,6 +50,33 @@ describe('setup store', () => {
   })
 
   it('state is reactive', () => {
+    const store = useStore()
+    const upperCased = computed(() => store.name.toUpperCase())
+    expect(upperCased.value).toBe('YOUNG')
+    store.name = 'y'
+    expect(upperCased.value).toBe('Y')
+  })
+})
+
+// TODO: 对composition api的支持
+describe('setup store with composition api', () => {
+  beforeEach(() => {
+    setActivePinia(createPinia())
+  })
+
+  const useStore = defineStore('main', () => {
+    const name = ref('young')
+    return { name }
+  })
+
+  it.skip('can also access state', () => {
+    const store = useStore()
+    expect(store.name).toBe('young')
+    store.name = 'abc'
+    expect(store.name).toBe('abc')
+  })
+
+  it.skip('state is reactive', () => {
     const store = useStore()
     const upperCased = computed(() => store.name.toUpperCase())
     expect(upperCased.value).toBe('YOUNG')
