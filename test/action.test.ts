fb53a9a6bf344b3299573aba3becc728afaeef99
cuiyiming1998 <1269567664@qq.com>
2023-04-07 15:02:20 +0800

    tests: 添加action test

--- test/action.test.ts
+++ test/action.test.ts
@@ -1,7 +1,8 @@
 import { beforeEach, describe, expect, it } from 'vitest'
 import { createPinia, defineStore, setActivePinia } from '../src'
+import { ref, unref } from 'vue-demi'
 
-describe('getters', () => {
+describe('actions', () => {
   beforeEach(() => {
     setActivePinia(createPinia())
   })
@@ -25,3 +26,27 @@ describe('getters', () => {
     expect(store.foo).toBe(false)
   })
 })
+
+describe('actions with setup options', () => {
+  beforeEach(() => {
+    setActivePinia(createPinia())
+  })
+
+  const useStore = defineStore('main', () => {
+    const foo = ref(true)
+
+    const toggle = () => {
+      foo.value = !unref(foo)
+    }
+
+    return { foo, toggle }
+  })
+
+  it('should call actions', () => {
+    const store = useStore()
+    expect(store.foo).toBe(true)
+    store.toggle()
+    expect(store.foo).toBe(false)
+  })
+})
+
